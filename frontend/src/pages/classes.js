83c207469f14c86a2fa893e9c784d128d345fae8
Tommy <skywriter@gmail.com>
2020-12-31 11:15:40 +0000

    Updated Classes Page

--- frontend/src/pages/classes.js
+++ frontend/src/pages/classes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import BannerClasses from '../components/BannerClasses';
 
 import ladiesboxfit from '../assets/images/ladies-boxfit-class.jpg';
