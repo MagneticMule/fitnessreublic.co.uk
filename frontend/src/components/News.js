81b4cc24e3f52deee7b8a5c76b47e07f49a83f08
Tommy <skywriter@gmail.com>
2021-02-23 21:32:32 +0000

    (add) news section to landing page

--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -5,13 +5,13 @@ import styled from 'styled-components';
 
 const NewsSection = styled.section`
 
+border: 1rem solid purple;
 padding: 4rem;
 background: rgb(255,179,0);
 background: linear-gradient(315deg, rgba(255,179,0,1) 0%, rgba(189,0,255,1) 100%);
 
 color: white;
     h1 {
-
       text-shadow: 0 1px 0 rgba(0,0,0,.2),0 2px 8px rgba(0,0,0,.7);
       font-size: 4rem;
       font-weight: 900;
@@ -26,40 +26,56 @@ color: white;
       color: white;
     }
 
-    p{ color: white;}
+    p { color: white;}
 
     strong {
       color: white;
+    }
+
+    span {
+      text-transform: uppercase;
+      color: black;
+      font-weight: 900;
+      background: orange;
+      padding: .2rem .3rem;
+    }
+
+    a {
+      font-weight: 900;
+      border-radius: 8px;
+      list-style:none;
+      font-size: 1rem;
+
     }
 `;
 
 
 const News = () => {
   return (
-
     <NewsSection>
        <section id="news" className="news" >
               <h1>WE ARE ALMOST THERE!</h1>
               <h2>With an end date now firmly in sight, we are ready to help you get back into our gym</h2>
-              <h2>10% OFF MEMBERSHIP FEE'S</h2>
-              <h3>This Lockdown we want you</h3>
+              <strong>We hope you are still enjoying our online classes!?</strong>
               <p>
-                We are offering <strong>10% off our range of memberships each month</strong> when you pay by direct debit.
-                <br/>
-                Remember at Fitness Republic there are <strong>no Joining Fee's</strong> and <strong>no contracts</strong>.
+              Of course, We understand that training at home or outside is great but not for everyone. It can be difficult to motivate yourself to train and eat right. At <strong>Fitness Republic</strong> you can train in a <strong>strong, safe and friendly</strong> environment with a lovely inclusive family feel.
               </p>
-              <ul>
+              <p>
+              With <span>state of the art equipment</span>, <span>a diverse range of classes</span>, <span>approachable staff</span> and all with our unique community feel, <strong>Fitness Republic</strong> is still <strong>the best place</strong> to <span>workout 💪 </span><strong> + </strong> <span>FEEL GOOD!👍</span> in 2021 and beyond!</p>
+
+              <h3>Ready to kickstart your fitness in April 2021?</h3>
+              <ul className="actions">
                 <li>
                   <Link
                     aria-label="Link to Membership Plans Page"
-                    to="/membership#"
-                    className="button special"
+                    to="/membership"
+                    className="button special arrow"
                   >
-                    JOIN NOW
+                    JOIN or RENEW NOW
                   </Link>
                 </li>
               </ul>
-              <p>Join now and pay nothing until 15th March 2021</p>
+
 
           </section>
           </NewsSection>
