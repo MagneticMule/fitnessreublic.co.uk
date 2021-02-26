7392cea6f9c385c3f0ca1afc52915ecf64a058e1
Tommy <skywriter@gmail.com>
2021-02-26 15:30:43 +0000

    (fix) footer contact from was not working with Netlify forms

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -5,35 +5,36 @@ const Contact = (props) => (
     <section id="contact">
         <div className="inner">
             <section>
-                    <form name="Contact" method="post" data-netlify="true"  data-netlify-honeypot="bot-field" action="#">
+                <form name="Contact" method="post" data-netlify="true"  data-netlify-honeypot="bot-field" action="#">
 
-                    <input type="hidden" name="bot-field" />
-                    <input type="hidden" name="form-name" value="General Question" />
+                <input type="hidden" name="bot-field" />
+                <input type="hidden" name="form-name" value="General Question" />
 
-                    <div className="field half first">
-                        <label htmlFor="name">Name</label>
-                        <input type="text" name="name" id="name" />
-                    </div>
+                <div className="field half first">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" id="name" />
+                </div>
 
-                    <div className="field half">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" name="email" id="email" />
-                    </div>
+                <div className="field half">
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name="email" id="email" />
+                </div>
 
-                    <div className="field">
-                        <label htmlFor="phone">Telephone Number (optional)</label>
-                        <input type="tel" name="phone" id="phone" />
-                    </div>
+                <div className="field">
+                    <label htmlFor="phone">Telephone Number (optional)</label>
+                    <input type="tel" name="phone" id="phone" />
+                </div>
 
-                    <div className="field">
-                        <label htmlFor="message">Message</label>
-                        <textarea name="message" id="message" rows="6"></textarea>
-                    </div>
+                <div className="field">
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" id="message" rows="6"></textarea>
+                </div>
 
-                    <ul className="actions">
-                        <li><input type="submit" value="Send Message" className="special" /></li>
-                        <li><input type="reset" value="Clear" /></li>
-                    </ul>
+                <ul className="actions">
+                    <li>
+                        <input type="submit" value="Send Message" className="special" /></li>
+
+                </ul>
                 </form>
             </section>
             <section className="split">
@@ -41,9 +42,9 @@ const Contact = (props) => (
                 </section>
                 <section>
                     <div className="contact-method">
-                        <span className="icon alt fa-email"></span>
+                        <span className="icon alt fa-mail"></span>
                         <h3>Email</h3>
-                        <span>info@fitnessrepublic.co.uk</span>
+                        <span><a href="mailto:info@fitnessrepublic.co.uk">info@fitnessrepublic.co.uk</a></span>
                     </div>
                     <div className="contact-method">
                         <span className="icon alt fa-phone"></span>
