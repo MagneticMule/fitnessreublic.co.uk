b8dd40690c0fea9ae567375ca701d956f8ae8d87
Tommy <skywriter@gmail.com>
2021-04-28 11:14:43 +0100

    (fix) Removed cookie consent banner due to user complaints that it would not close on mobile

--- frontend/src/components/layout.js
+++ frontend/src/components/layout.js
@@ -50,21 +50,6 @@ class Layout extends React.Component {
                     <Footer />
                 </div>
                     <Menu onToggleMenu={this.handleToggleMenu} />
-
-                    <CookieConsent
-                        buttonClasses="button special"
-                        containerClasses="alert alert-warning col-lg-12"
-                        contentClasses="text-capitalize"
-                        location="bottom"
-                        buttonText="X"
-                        declineButtonText="Decline"
-                        cookieName="gatsby-gdpr-google-analytics">
-                            <div className="inner">
-                            <p>We store cookies on your computer. These cookies are used to collect information about how you interact with this website and allow us to remember you.
-                            We use this information in order to improve and customize your browsing experience and for analytics and metrics about our visitors on this website.</p>
-                            <p>By using this website you agree for us to store cookies on your local computer.</p>
-                            </div>
-                        </CookieConsent>
                         <WhatsAppWidget
                             companyName='Fitness Republic Ashby'
                             phoneNumber='+441530413330' textReplyTime='We Typically get back to you within the hour'
